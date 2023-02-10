314086b31518cd30dc7501eb5bbafe9283b30eee
SergioBogaro <sergio.bogaro@smartinnovation.com.br>
2023-02-10 15:13:50 -0300

    pagina de niveis

--- src/pages/administracao/niveis.tsx
+++ src/pages/administracao/niveis.tsx
@@ -11,17 +11,17 @@ import { Table } from '@ui/Table';
 import type { NextPage } from 'next';
 const tableColumns = [
   { heading: 'Nome', value: 'nome' },
-  { heading: 'Nivel', value: 'nivel' },
-  { heading: 'Login', value: 'email' },
+  { heading: 'Qtd de Usuários', value: 'nivel' },
+  { heading: 'Observações', value: 'email' },
   { heading: '', value: 'opcoes' }
 ];
-const Users: NextPage = () => {
-  const [userData, setUserData] = useState([]);
+const Niveis: NextPage = () => {
+  const [levelData, setLevelData] = useState([]);
   const [searchFilter, setSearchFilter] = useState('');
   const [newSearch, setNewSearch] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
 
-  function userTableOption(user: any) {
+  function levelTableOption(user: any) {
     return (
       <div>
         <h2>{user.nome}</h2>
@@ -34,48 +34,48 @@ const Users: NextPage = () => {
     setNewSearch(true);
   };
 
-  useEffect(() => {
-    const page = '0';
-    setNewSearch(false);
-    setIsLoading(true);
-    searchUsers(searchFilter, page)
-      .then((res) => {
-        const table = res.data.data.map((user: any) => ({
-          nome: user.nome,
-          nivel: user.papel.nome,
-          email: user.email,
-          opcoes: userTableOption(user)
-        }));
+  //   useEffect(() => {
+  //     const page = '0';
+  //     setNewSearch(false);
+  //     setIsLoading(true);
+  //     searchUsers(searchFilter, page)
+  //       .then((res) => {
+  //         const table = res.data.data.map((user: any) => ({
+  //           nome: user.nome,
+  //           nivel: user.papel.nome,
+  //           email: user.email,
+  //           opcoes: levelTableOption(user)
+  //         }));
 
-        setUserData(table);
-        setIsLoading(false);
-      })
-      .catch((err) => {
-        console.log(err);
-        setIsLoading(false);
-      });
-  }, [newSearch]);
+  //         setLevelData(table);
+  //         setIsLoading(false);
+  //       })
+  //       .catch((err) => {
+  //         console.log(err);
+  //         setIsLoading(false);
+  //       });
+  //   }, [newSearch]);
 
   return (
     <div className="mx-auto flex w-11/12 flex-col gap-4 rounded p-5">
       <div className="flex justify-between">
-        <h2 className="text-xl font-bold">Lista de Usuários</h2>
+        <h2 className="text-xl font-bold">Lista de Níveis</h2>
         <ButtonOrLink intent={'secondary'}>
-          <GoPlus /> Novo Usuário
+          <GoPlus /> Novo Nível
         </ButtonOrLink>
       </div>
 
       <form className="flex gap-2" onSubmit={filterTable}>
-        <Input label="" placeholder="Pesquisar Usuário" value={searchFilter} onChange={(e) => setSearchFilter(e.target.value)} />
+        <Input label="" placeholder="Pesquisar Nível" value={searchFilter} onChange={(e) => setSearchFilter(e.target.value)} />
         <ButtonOrLink intent={'secondary'} type={'submit'}>
           <HiOutlineMagnifyingGlass />
         </ButtonOrLink>
       </form>
 
-      <Table collumns={tableColumns} data={userData} />
+      <Table collumns={tableColumns} data={levelData} />
       <Loading isLoading={isLoading} />
     </div>
   );
 };
 
-export default Users;
+export default Niveis;
