f1d7f7f7fd61476e0e01f4b9e2678a7f7ae213f6
SergioBogaro <sergio.bogaro@smartinnovation.com.br>
2023-04-12 10:09:23 -0300

    Change folder name

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import '../../styles/globals.css';
 
 import { ThemeProvider } from 'next-themes';
 
-import Layout from '../components/specific/layout';
+import Layout from '../components/layout/layout';
 import { UserProvider } from '../contexts/auth';
 
 import type { AppProps } from 'next/app';
